fb34530335e957e43e83d763c8b331e082efd6d1
Lucas <arrizabalucas@gmail.com>
2023-03-29 11:44:13 +0200

    Update

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,4 +1,4 @@
-import { DataSource, DataSourceOptions } from "typeorm";
+import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 import Comment from "./entities/comment";
 import File from "./entities/file";
